3c84018d33e3eb7157b2d189ebcbddb6893d3598
ashivaram <ashivaram123@gmail.com>
2020-09-15 09:21:37 +0530

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int i = 1; i < 20; i++)
+            {
+                
+                if(i%3 == 0)
+                {
+                    sum = sum + i;
+                }
+                
+            }
+            Console.WriteLine($"Addition of integers is  { sum}");
+            Console.ReadKey();
+        }
+       
+    }
+}
